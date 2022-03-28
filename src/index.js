a48dbfae2c438b621c4f7674dcada96435f07de6
Julissa Huaman Hilari <julissa.2606@gmail.com>
2022-03-27 21:53:01 -0500

    responsive

--- src/index.js
+++ src/index.js
@@ -1,38 +1,33 @@
 import cipher from "./cipher.js";
 
 //*TODO - OBTENIENDO LOS ELEMENTOS, ETIQUETAS O SECCIONES
-//* obteniendo el header
-let viewHeader = document.getElementsByClassName("header")[0];
+//* obteniendo el Bienvenida
+let viewBienvenida = document.getElementsByClassName("bienvenida")[0];
 //* boton  para denunciar registrando datos */
 let btnConDatos = document.getElementById("condatos");
-
-//* boton descifrar */
-let btndesencrypt = document.getElementById("btn-descifrar");
 
 //* obteniendo el mensaje del offset
 let offsetMessage = document.getElementsByClassName("mensaje-offset")[0];
 
 //*todo EVENTO PARA EL BOTON SIGUIENTE
-document.getElementById("btn-denunciacd").addEventListener("click", ()=>{
-  alert("gol")
-  document.getElementsByClassName("denuncia")[0].style.display="block";
-  document.getElementsByClassName("registro-denuncia")[0].style.display="none";
-  document.getElementsByClassName("datos")[0].style.display="none";
-  document.getElementsByClassName("casocd")[0].style.display="flex";
-})
+// document.getElementById("btn-denunciacd").addEventListener("click", ()=>{
+//     document.getElementsByClassName("denuncia")[0].style.display="block";
+//   document.getElementsByClassName("registro-denuncia")[0].style.display="none";
+//   document.getElementsByClassName("datos")[0].style.display="none";
+//   document.getElementsByClassName("casocd")[0].style.display="flex";
+// })
 
 //*todo EVENTO PARA EL BOTON ANTERIOR
-document.getElementById("btn-registrocd").addEventListener("click", ()=>{
-  alert("gol")
-  document.getElementsByClassName("denuncia")[0].style.display="block";
-  document.getElementsByClassName("registro-denuncia")[0].style.display="none";
-  document.getElementsByClassName("datos")[0].style.display="block";
-  document.getElementsByClassName("casocd")[0].style.display="none";
-})
+// document.getElementById("btn-registrocd").addEventListener("click", ()=>{
+//   document.getElementsByClassName("denuncia")[0].style.display="block";
+//   document.getElementsByClassName("registro-denuncia")[0].style.display="none";
+//   document.getElementsByClassName("datos")[0].style.display="block";
+//   document.getElementsByClassName("casocd")[0].style.display="none";
+// })
 
 //*todo EVENTO PARA VOLVER AL INICIO
 document.getElementById("btn-inicio").addEventListener("click",()=>{
-  viewHeader.style.display="flex";
+  viewBienvenida.style.display="flex";
   document.querySelector(".principal").style.display="none";
 })
 
@@ -40,19 +35,24 @@ document.getElementById("btn-inicio").addEventListener("click",()=>{
 document.getElementById("btn-limpiar").addEventListener("click",limpiar);
 
 function limpiar(){
-  let datosInput= document.querySelectorAll(".data");
+  let datosInput= document.querySelectorAll(".dataOfset");
+  let datosForm=document.querySelectorAll(".data");
   for (let i = 0; i < datosInput.length; i++) {
     datosInput[i].value="";
+  }
+  for (let index = 0; index < datosForm.length; index++) {
+    datosForm[index].value="";
+    
   }
 }
 //*TODO - EVENTO PARA DENUNCIAR REGISTRANDO TUS DATOS */
 btnConDatos.addEventListener("click", ()=>{
-  viewHeader.style.display="none";
-  document.querySelector(".principal").style.display="block";
+  viewBienvenida.style.display="none";
+  document.querySelector(".principal").style.display="flex";
 });
 
 // *todo EVENTO PARA EL ACORDEON
-document.getElementsByClassName("exp")[0].addEventListener("click", function() {
+document.getElementsByClassName("titulo-instrucciones")[0].addEventListener("click", function() {
     this.classList.toggle("active");
     let panel = this.nextElementSibling;
     if (panel.style.maxHeight) {
@@ -63,19 +63,16 @@ document.getElementsByClassName("exp")[0].addEventListener("click", function() {
   });
 
 
-//*todo EVENTO PARA EL BOTON 
-
 //*todo EVENTO PARA CIFRAR
-document.querySelector("#btn-encrypt").addEventListener("click",()=>{
+document.getElementById("btn-encrypt").addEventListener("click",()=>{
   //* obteniendo el offset
   let desplazamiento = document.getElementById("numoffset").value;
   //* convirtiendo el offset a number    
   let offsetNumber = Number(desplazamiento);
   
   if(offsetNumber!==0){
-    
-  //* si offset es diferente de cero entonces el borde de la caja del input es verde
-    document.getElementById("numoffset").style.border="solid 1px #00808080";
+    //* si offset es diferente de cero entonces el borde de la caja del input es verde
+    document.getElementById("numoffset").style.border="solid 1px green";
     offsetMessage.innerHTML="";
 
     //obteniendo valores de los input
@@ -85,7 +82,7 @@ document.querySelector("#btn-encrypt").addEventListener("click",()=>{
     let dni=document.getElementById("numdoc").value;
     let telefono=document.getElementById("phone").value;
     let correo=document.getElementById("email").value;
-
+    let direccion=document.getElementById("direccion").value;
     //* cifrando los datos de los input
     document.getElementById("name").value=cipher.encode(offsetNumber,nombres)
     document.getElementById("lastnamepat").value=cipher.encode(offsetNumber,apellidoPaterno);
@@ -93,45 +90,45 @@ document.querySelector("#btn-encrypt").addEventListener("click",()=>{
     document.getElementById("numdoc").value=cipher.encode(offsetNumber,dni);
     document.getElementById("phone").value=cipher.encode(offsetNumber,telefono);
     document.getElementById("email").value=cipher.encode(offsetNumber,correo);
+    document.getElementById('direccion').value=cipher.encode(offsetNumber,direccion);
   }
 
   else{
       offsetMessage.innerHTML="Ingrese un número diferente de cero."
       offsetMessage.style.color="#ff0000";
-      document.getElementById("offset").style.border="solid 1px #ff0000";
+      document.getElementById("numoffset").style.border="solid 1px red";
   }
 })
 
 
 
 //*? evento cuando doy click en boton desencrypt
-
-btndesencrypt.addEventListener("click", ()=> {
+//* boton descifrar */
+document.getElementById("btn-descifrar").addEventListener("click", ()=> {
+  let des = document.getElementById("numoffset").value;
+  des=(Number(des));
+      
+  if (des>0) {
+  //obteniendo valores de los input
+    let nombresDes=document.getElementById("name").value;
+    let apellidoPaternoDes= document.getElementById("lastnamepat").value;
+    let apellidoMaternoDes= document.getElementById("lastnamemat").value;
+    let dniDes=document.getElementById("numdoc").value;
+    let telefonoDes=document.getElementById("phone").value;
+    let correoDes=document.getElementById("email").value;
+    let direccionDes=document.getElementById("direccion").value;
+    document.getElementById("name").value=cipher.decode(des,nombresDes);
+    document.getElementById("lastnamepat").value= cipher.decode(des,apellidoPaternoDes);
+    document.getElementById("lastnamemat").value= cipher.decode(des,apellidoMaternoDes);
+    document.getElementById("numdoc").value=cipher.decode(des,dniDes);
+    document.getElementById("phone").value=cipher.decode(des,telefonoDes);
+    document.getElementById("email").value=cipher.decode(des,correoDes);
+    document.getElementById("direccion").value=cipher.decode(des,direccionDes);
+  }
   
-  document.getElementById("btn-descifrar").addEventListener("click",()=>{
-    let des = document.getElementById("numoffset").value;
-    des=(Number(des));
-    // console.log(des)
-    
-    if (des>0) {
-    //obteniendo valores de los input
-      let nombresDes=document.getElementById("name").value;
-      let apellidoPaternoDes= document.getElementById("lastnamepat").value;
-      let apellidoMaternoDes= document.getElementById("lastnamemat").value;
-      let dniDes=document.getElementById("numdoc").value;
-      let telefonoDes=document.getElementById("phone").value;
-      let correoDes=document.getElementById("email").value;
-      document.getElementById("name").value=cipher.decode(des,nombresDes);
-      document.getElementById("lastnamepat").value= cipher.decode(des,apellidoPaternoDes);
-      document.getElementById("lastnamemat").value= cipher.decode(des,apellidoMaternoDes);
-      document.getElementById("numdoc").value=cipher.decode(des,dniDes);
-      document.getElementById("phone").value=cipher.decode(des,telefonoDes);
-      document.getElementById("email").value=cipher.decode(des,correoDes);
-    }
-  
-  });
+});
 
-})
+
 
 //*todo EVENTO PARA CREAR LA FILA
 let btnagregar = document.querySelector(".agregar-denunciadocd");
@@ -149,8 +146,3 @@ btnagregar.addEventListener("click", ()=>{
     
 });
 
-
-    
-    // eslint-disable-next-line no-undef
-    // console.log(cipher);
-    // console.log(cipher.decode("hola"));
